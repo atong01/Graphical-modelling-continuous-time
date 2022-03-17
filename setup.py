17ece7caa756e0ffa91b91471610e84a50cccbd2
Alex Tong <alexandertongdev@gmail.com>
2022-03-17 09:25:16 -0400

    Reformat to a package, first round of cleanup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ngm",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Neural Graphical Model from https://arxiv.org/pdf/2105.02522.pdf",
+)
